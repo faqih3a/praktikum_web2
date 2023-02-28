fae39153904c525fd7e1aa2dd9fe424ce4d474e5
faqih3a <ahmadfaqih3a@gmail.com>
2023-02-28 15:18:41 +0700

    pertemuan 2/form pendataan nilai mahasiswa

---
+++ praktikum2/proses_nilai.php
@@ -0,0 +1,17 @@
+<?php 
+$nama = $_GET['name'];
+$matkul = $_GET['matkul'];
+$UTS = $_GET['UTS'];
+$UAS = $_GET['UAS'];
+$TUGAS = $_GET['Tugas'];
+$total_nilai = ($TUGAS + $UTS + $UAS) / 3;
+
+if($total_nilai > 75){
+    $keterangan = "LULUS";
+}
+else{
+    $keterangan = "Tidak LULUS";
+}
+
+
+?>
